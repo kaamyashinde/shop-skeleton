e83a9b183129af3abea89842077b282d4f83975e
Kaamya Shinde <kaamyashinde@gmail.com>
2026-09-17 23:31:49 +0200

    Move api.ts to api subfolder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { fetchProducts } from './api'
+import { fetchProducts } from './api/api'
 import Cart from './components/Cart'
 import ProductList from './components/ProductList'
 import type { Product } from './types'
